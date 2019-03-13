c6984585a5405a5637a653b68f804baddf4a4e3b
ljt <765091375@qq.com>
2019-03-13 19:56:53 +0800

    创建数据库和表的各项配置

---
+++ app/src/main/java/com/ljt/coolweather/LitePalApplication.java
@@ -0,0 +1,10 @@
+package com.ljt.coolweather;
+
+import android.app.Application;
+
+public class LitePalApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
